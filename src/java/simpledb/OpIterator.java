ab6fd93ee7867393d4f3e68845129a7d154b1b6d
joker452 <dbw15@mails.tsinghua.edu.cn>
2019-05-15 21:28:54 +0800

    finish lab1

--- src/java/simpledb/OpIterator.java
+++ src/java/simpledb/OpIterator.java
@@ -1,4 +1,5 @@
 package simpledb;
+
 import java.io.Serializable;
 import java.util.*;
 
@@ -10,47 +11,52 @@ import java.util.*;
  * child iterator open methods, and in a close method, an iterator
  * should call its children's close methods.
  */
-public interface OpIterator extends Serializable{
-  /**
-   * Opens the iterator. This must be called before any of the other methods.
-   * @throws DbException when there are problems opening/accessing the database.
-   */
-  public void open()
-      throws DbException, TransactionAbortedException;
+public interface OpIterator extends Serializable {
+    /**
+     * Opens the iterator. This must be called before any of the other methods.
+     *
+     * @throws DbException when there are problems opening/accessing the database.
+     */
+    public void open()
+            throws DbException, TransactionAbortedException;
 
-  /** Returns true if the iterator has more tuples.
-   * @return true f the iterator has more tuples.
-   * @throws IllegalStateException If the iterator has not been opened
- */
-  public boolean hasNext() throws DbException, TransactionAbortedException;
+    /**
+     * Returns true if the iterator has more tuples.
+     *
+     * @return true if the iterator has more tuples.
+     * @throws IllegalStateException If the iterator has not been opened
+     */
+    public boolean hasNext() throws DbException, TransactionAbortedException;
 
-  /**
-   * Returns the next tuple from the operator (typically implementing by reading
-   * from a child operator or an access method).
-   *
-   * @return the next tuple in the iteration.
-   * @throws NoSuchElementException if there are no more tuples.
-   * @throws IllegalStateException If the iterator has not been opened
-   */
-  public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException;
+    /**
+     * Returns the next tuple from the operator (typically implementing by reading
+     * from a child operator or an access method).
+     *
+     * @return the next tuple in the iteration.
+     * @throws NoSuchElementException if there are no more tuples.
+     * @throws IllegalStateException  If the iterator has not been opened
+     */
+    public Tuple next() throws DbException, TransactionAbortedException, NoSuchElementException;
 
-  /**
-   * Resets the iterator to the start.
-   * @throws DbException when rewind is unsupported.
-   * @throws IllegalStateException If the iterator has not been opened
-   */
-  public void rewind() throws DbException, TransactionAbortedException;
+    /**
+     * Resets the iterator to the start.
+     *
+     * @throws DbException           when rewind is unsupported.
+     * @throws IllegalStateException If the iterator has not been opened
+     */
+    public void rewind() throws DbException, TransactionAbortedException;
 
-  /**
-   * Returns the TupleDesc associated with this OpIterator.
-   * @return the TupleDesc associated with this OpIterator.
-   */
-  public TupleDesc getTupleDesc();
+    /**
+     * Returns the TupleDesc associated with this OpIterator.
+     *
+     * @return the TupleDesc associated with this OpIterator.
+     */
+    public TupleDesc getTupleDesc();
 
-  /**
-   * Closes the iterator. When the iterator is closed, calling next(),
-   * hasNext(), or rewind() should fail by throwing IllegalStateException.
-   */
-  public void close();
+    /**
+     * Closes the iterator. When the iterator is closed, calling next(),
+     * hasNext(), or rewind() should fail by throwing IllegalStateException.
+     */
+    public void close();
 
 }
--- src/java/simpledb/SeqScan.java
+++ src/java/simpledb/SeqScan.java
@@ -1,5 +1,6 @@
 package simpledb;
 
+import javax.xml.crypto.Data;
 import java.util.*;
 
 /**
@@ -10,59 +11,68 @@ import java.util.*;
 public class SeqScan implements OpIterator {
 
     private static final long serialVersionUID = 1L;
+    private final TransactionId tid;
+    private int tableid;
+    private String tableAlias;
+    private TupleDesc tdWithPrefix = null;
+    private boolean isOpen = false;
+    private DbFileIterator it = null;
 
     /**
      * Creates a sequential scan over the specified table as a part of the
      * specified transaction.
      *
-     * @param tid
-     *            The transaction this scan is running as a part of.
-     * @param tableid
-     *            the table to scan.
-     * @param tableAlias
-     *            the alias of this table (needed by the parser); the returned
-     *            tupleDesc should have fields with name tableAlias.fieldName
-     *            (note: this class is not responsible for handling a case where
-     *            tableAlias or fieldName are null. It shouldn't crash if they
-     *            are, but the resulting name can be null.fieldName,
-     *            tableAlias.null, or null.null).
+     * @param tid        The transaction this scan is running as a part of.
+     * @param tableid    the table to scan.
+     * @param tableAlias the alias of this table (needed by the parser); the returned
+     *                   tupleDesc should have fields with name tableAlias.fieldName
+     *                   (note: this class is not responsible for handling a case where
+     *                   tableAlias or fieldName are null. It shouldn't crash if they
+     *                   are, but the resulting name can be null.fieldName,
+     *                   tableAlias.null, or null.null).
      */
     public SeqScan(TransactionId tid, int tableid, String tableAlias) {
         // some code goes here
+        this.tid = tid;
+        this.tableid = tableid;
+        this.tableAlias = tableAlias;
     }
 
     /**
-     * @return
-     *       return the table name of the table the operator scans. This should
-     *       be the actual name of the table in the catalog of the database
-     * */
+     * @return return the table name of the table the operator scans. This should
+     * be the actual name of the table in the catalog of the database
+     */
     public String getTableName() {
-        return null;
+        return Database.getCatalog().getTableName(tableid);
     }
 
     /**
      * @return Return the alias of the table this operator scans.
-     * */
-    public String getAlias()
-    {
+     */
+    public String getAlias() {
         // some code goes here
-        return null;
+        return tableAlias;
     }
 
     /**
      * Reset the tableid, and tableAlias of this operator.
-     * @param tableid
-     *            the table to scan.
-     * @param tableAlias
-     *            the alias of this table (needed by the parser); the returned
-     *            tupleDesc should have fields with name tableAlias.fieldName
-     *            (note: this class is not responsible for handling a case where
-     *            tableAlias or fieldName are null. It shouldn't crash if they
-     *            are, but the resulting name can be null.fieldName,
-     *            tableAlias.null, or null.null).
+     *
+     * @param tableid    the table to scan.
+     * @param tableAlias the alias of this table (needed by the parser); the returned
+     *                   tupleDesc should have fields with name tableAlias.fieldName
+     *                   (note: this class is not responsible for handling a case where
+     *                   tableAlias or fieldName are null. It shouldn't crash if they
+     *                   are, but the resulting name can be null.fieldName,
+     *                   tableAlias.null, or null.null).
      */
     public void reset(int tableid, String tableAlias) {
         // some code goes here
+        // isOpen = false?
+        this.tableid = tableid;
+        this.tableAlias = tableAlias;
+        tdWithPrefix = null;
+        it = null;
+        isOpen = false;
     }
 
     public SeqScan(TransactionId tid, int tableId) {
@@ -71,6 +81,9 @@ public class SeqScan implements OpIterator {
 
     public void open() throws DbException, TransactionAbortedException {
         // some code goes here
+        it = Database.getCatalog().getDatabaseFile(tableid).iterator(tid);
+        it.open();
+        isOpen = true;
     }
 
     /**
@@ -81,30 +94,55 @@ public class SeqScan implements OpIterator {
      * (e.g., "alias.fieldName").
      *
      * @return the TupleDesc with field names from the underlying HeapFile,
-     *         prefixed with the tableAlias string from the constructor.
+     * prefixed with the tableAlias string from the constructor.
      */
     public TupleDesc getTupleDesc() {
         // some code goes here
-        return null;
+        if (isOpen) {
+            if (tdWithPrefix == null) {
+                TupleDesc td = Database.getCatalog().getTupleDesc(tableid);
+                String prefix = (tableAlias == null) ? "null" : tableAlias;
+                int numFields = td.numFields();
+                Type[] typeAr = new Type[numFields];
+                String[] fieldAr = new String[numFields];
+                for (int i = 0; i < numFields; ++i) {
+                    typeAr[i] = td.getFieldType(i);
+                    fieldAr[i] = prefix + "." + td.getFieldName(i);
+                }
+                tdWithPrefix = new TupleDesc(typeAr, fieldAr);
+            }
+            return tdWithPrefix;
+        }
+        throw new IllegalStateException("SeqScan iterator is not open!");
     }
 
     public boolean hasNext() throws TransactionAbortedException, DbException {
         // some code goes here
-        return false;
+        if (isOpen)
+            return it.hasNext();
+        throw new IllegalStateException("SeqScan iterator is not open!");
     }
 
     public Tuple next() throws NoSuchElementException,
             TransactionAbortedException, DbException {
         // some code goes here
-        return null;
+        if (isOpen)
+            return it.next();
+        throw new IllegalStateException("SeqScan iterator is not open!");
     }
 
     public void close() {
         // some code goes here
+        it.close();
+        isOpen = false;
     }
 
     public void rewind() throws DbException, NoSuchElementException,
             TransactionAbortedException {
         // some code goes here
+        if (isOpen)
+            it.rewind();
+        else
+            throw new IllegalStateException("SeqScan iterator is not open!");
     }
 }
